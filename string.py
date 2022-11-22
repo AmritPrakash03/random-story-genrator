046a0384df36405a13e85ecce573eb77ea4db8be
AmritPrakash03 <118277690+AmritPrakash03@users.noreply.github.com>
2022-11-22 18:02:52 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,5 @@
+import random
+who=[" amrit"," shabd"," anant"]
+happend=[" ate a burger"," went to delhi"," went to the beaches of goa"]
+when=["once","10 days ago","sometime ago"]
+print(random.choice(when)+" a person named"+random.choice(who)+random.choice(happend))
